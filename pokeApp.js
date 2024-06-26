1c23d22c5ff22e1e4e3dfb1e7c336757f855a116
JuditAF <idryscreep@gmail.com>
2024-06-26 20:20:30 +0200

    subo creación de archivos para el proyecto

---
+++ pokeApp.js
@@ -0,0 +1,28 @@
+
+class Pokemon {
+
+    constructor (nombre, imagen, habilidades) {
+
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.habilidades = habilidades;
+    }
+};
+
+function postPokemon (nombre, imagen, habilidades) {
+
+    let newPokemon = new Pokemon (document.getElementById("nombre"), 
+                                  document.getElementById("imagen"),
+                                  document.getElementById("habilidades")
+                                );
+
+    console.log(JSON.stringify(newPokemon));
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", "http://localhost:3000/poke", true);
+    xhttp.setRequestHeader("Content-Type", "aplication/json");
+    xhttp.send(JSON.stringify(newPokemon));
+};
+
+function getPokemon () {};
